5e94ed5ef2283a3a2b0ebfbb3b7531a2dab3aded
Ravichandrika-github <ravichandrika.chitti@gmail.com>
2024-01-20 10:13:37 +0530

    adding constants

---
+++ assignment-problem/node-app/constants.js
@@ -0,0 +1,8 @@
+const PI = 3.14159; const AGE = 30;
+const USERNAME = "David";
+const USER = {
+ name: "Billy",
+ age: 40 
+};
+
+export { PI, AGE, USERNAME, USER as default };
--- assignment-problem/node-app/server.js
+++ assignment-problem/node-app/server.js
@@ -1,8 +1,14 @@
+import * as constants from './constants';
+
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
+
+  console.log(constants.USERNAME); // David
+  console.log(constants.default); // { name: "Billy", age: 40 }
+  console.log(constants.default.age); // 40
   res.send(`
     <h1>Hello from inside the very basic Node app!</h1>
   `);
